9c974832fb7247766ef6c6720242b599f152a7fe
Razvan Alexandru Pricopie <Pricopie.Ma.Razvan@student.utcluj.ro>
2024-01-17 13:29:25 +0200

    cdkmenubar added to toolbar cart button

--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -3,17 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
-
-
+import { CdkMenuModule } from '@angular/cdk/menu';
 
 @NgModule({
-  declarations: [
-    LayoutComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    SharedModule
-  ]
+  declarations: [LayoutComponent],
+  imports: [CommonModule, RouterModule, SharedModule, CdkMenuModule],
 })
-export class LayoutModule { }
+export class LayoutModule {}
