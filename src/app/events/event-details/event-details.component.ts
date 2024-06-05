27db8d83e4bc9fc7198b05e4b07f7e041232fd76
razvanpricopie <pricopie.razvan98@gmail.com>
2024-06-05 16:43:47 +0300

    add like/dislike event functionality

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -2,8 +2,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { EventDetails } from 'src/app/core/models/event.model';
+import { Observable, Subject, Subscription, forkJoin, takeUntil } from 'rxjs';
+import { AccountService } from 'src/app/core/account/account.service';
+import {
+  EventDetails,
+  UserEventLikeStatus,
+  UserLikeEvent,
+} from 'src/app/core/models/event.model';
 import { CartService } from 'src/app/core/services/cart.service';
 import { EventService } from 'src/app/core/services/event.service';
 
@@ -19,6 +24,7 @@ export class EventDetailsComponent implements OnInit, OnDestroy {
 
   eventId: string;
   eventDetails: EventDetails;
+  userEventLikeStatus: UserEventLikeStatus;
 
   quantitySub: Subscription = new Subscription();
   quantityValueMultiplied: number = 0;
@@ -27,11 +33,17 @@ export class EventDetailsComponent implements OnInit, OnDestroy {
 
   loading: boolean = true;
 
+  isUserLoggedIn: boolean;
+  private userId: string;
+
+  private onDestroy = new Subject<void>();
+
   constructor(
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
     private eventService: EventService,
     private cartService: CartService,
+    private accountService: AccountService,
     private sanitizer: DomSanitizer
   ) {}
 
@@ -40,13 +52,14 @@ export class EventDetailsComponent implements OnInit, OnDestroy {
 
     this.form = this.formBuilder.group({ quantity: [0, Validators.min(0)] });
 
-    this.initSubscriptions();
+    this.checkUserStatus();
 
-    
+    this.initSubscriptions();
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+    this.onDestroy.next();
+    this.onDestroy.complete();
   }
 
   addTickets() {
@@ -60,16 +73,108 @@ export class EventDetailsComponent implements OnInit, OnDestroy {
     }
   }
 
-  private initSubscriptions() {
-    this.subscriptions.push(
+  likeEvent() {
+    if (
+      (this.isUserLoggedIn && this.userEventLikeStatus.isLiked == false) ||
+      this.userEventLikeStatus.userId != this.userId ||
+      this.userEventLikeStatus.eventId != this.eventId
+    ) {
+      return;
+    }
+
+    if (this.userEventLikeStatus.isLiked == null) {
+      const likedEvent: UserLikeEvent = {
+        userId: this.userId,
+        eventId: this.eventId,
+      };
+
+      this.eventService.likeEvent(likedEvent).subscribe(() => {
+        this.eventService
+          .getUserLikeEventStatus(this.userId, this.eventId)
+          .subscribe((userEventLikeStatus) => {
+            this.userEventLikeStatus = userEventLikeStatus;
+          });
+      });
+    } else if (this.userEventLikeStatus.isLiked) {
       this.eventService
-        .getEventDetails(this.eventId)
-        .subscribe((eventDetails) => {
-          this.eventDetails = eventDetails;
-          this.initEventImage();
-          this.loading = false;
-        })
-    );
+        .deleteUserLikeEventStatus(this.userEventLikeStatus.id)
+        .subscribe(() => {
+          this.eventService
+            .getUserLikeEventStatus(this.userId, this.eventId)
+            .subscribe((userEventLikeStatus) => {
+              this.userEventLikeStatus = userEventLikeStatus;
+            });
+        });
+    }
+  }
+
+  dislikeEvent() {
+    if (
+      (this.isUserLoggedIn && this.userEventLikeStatus.isLiked) ||
+      this.userEventLikeStatus.userId != this.userId ||
+      this.userEventLikeStatus.eventId != this.eventId
+    ) {
+      return;
+    }
+
+    if (this.userEventLikeStatus.isLiked == null) {
+      const dislikedEvent: UserLikeEvent = {
+        userId: this.userId,
+        eventId: this.eventId,
+      };
+      this.eventService.dislikeEvent(dislikedEvent).subscribe(() => {
+        this.eventService
+          .getUserLikeEventStatus(this.userId, this.eventId)
+          .subscribe((userEventLikeStatus) => {
+            this.userEventLikeStatus = userEventLikeStatus;
+          });
+      });
+    } else if (!this.userEventLikeStatus.isLiked) {
+      this.eventService
+        .deleteUserLikeEventStatus(this.userEventLikeStatus.id)
+        .subscribe(() => {
+          this.eventService
+            .getUserLikeEventStatus(this.userId, this.eventId)
+            .subscribe((userEventLikeStatus) => {
+              this.userEventLikeStatus = userEventLikeStatus;
+            });
+        });
+    }
+  }
+
+  private checkUserStatus() {
+    this.accountService.userAuthStatus$.subscribe((isUserLoggedIn) => {
+      this.isUserLoggedIn = isUserLoggedIn;
+    });
+
+    this.accountService.userDetails$.subscribe((userDetails) => {
+      this.userId = userDetails.userId;
+    });
+  }
+
+  private initSubscriptions() {
+    const observables: Observable<any>[] = [
+      this.eventService.getEventDetails(this.eventId),
+    ];
+
+    if (this.isUserLoggedIn) {
+      observables.push(
+        this.eventService.getUserLikeEventStatus(this.userId, this.eventId)
+      );
+    }
+
+    forkJoin(observables)
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe(([eventDetails, userEventLikeStatus]) => {
+        this.eventDetails = eventDetails;
+
+        if (this.isUserLoggedIn) {
+          this.userEventLikeStatus = userEventLikeStatus;
+        }
+
+        this.initEventImage();
+        this.loading = false;
+      });
 
     this.subscriptions.push(
       this.form.get('quantity')?.valueChanges.subscribe((value) => {
